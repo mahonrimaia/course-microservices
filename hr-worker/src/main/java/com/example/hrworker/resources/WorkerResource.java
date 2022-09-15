2d93e0b3cc61b4ac96d8a147872552418017d497
Mahonri Maia <mahonri.maia@gmail.com>
2022-09-15 10:12:35 -0300

    db config server

--- hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import com.example.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +22,8 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+//    @Value("${test.config}")
+//    private String testConfig;
 
     @Autowired
     private Environment env;
@@ -34,7 +33,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/config")
     public ResponseEntity<Void> getConfig() {
-        logger.info("CONFIG = " + testConfig);
+//        logger.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
     }
 
@@ -47,11 +46,11 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Thread.sleep(3000L);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
